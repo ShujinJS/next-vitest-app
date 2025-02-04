7d056b6d8b5ca0cd2cd37862cbeb88a87bea2436
ShujinJS <uguryldrmbjk@gmail.com>
2025-02-05 02:08:04 +0300

    auth page template update

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,4 +1,3 @@
-import FeedCard from "@/components/Feed/FeedCard/component";
 import FeedTemplate from "@/components/Feed/FeedTemplate/component";
 
 export default function Page() {
--- src/components/Button/component.tsx
+++ src/components/Button/component.tsx
@@ -1,14 +1,23 @@
 "use client";
+import "./style.scss";
 
 type ButtonProps = {
   text: string;
+  width?: string;
   onClick: (param?: boolean) => void;
 };
 
 const Button = (props: ButtonProps) => {
-  const { text, onClick } = props;
+  const { text, width = "w-full", onClick } = props;
 
-  return <button onClick={() => onClick}>{text}</button>;
+  return (
+    <button
+      className={`custom-btn p-3 rounded-md ${width}`}
+      onClick={() => onClick()}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
